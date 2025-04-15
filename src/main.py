1276bc253ec52a4f6a46e59232fce2a16bcb917d
ChrisMar0512 <christian.mar@utec.edu.pe>
2025-04-15 18:08:47 -0500

    Agregando funcion suma

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
+def calculate() -> float:
+    pass
+#Hola comentario
+
+def resta(a, b):
+    return a - b
+
 def dividir(a, b):
     if b == 0:
         return "Error: División por cero"
     return a / b
+
 def suma(a, b):
     return a + b
 
